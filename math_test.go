ba74e884c18daf3b3e97d2ad99f5c433d8e59509
Alladio <alladio_bonesso@hotmail.com>
2023-08-13 23:21:14 -0300

    Feat: cover anothers tests

--- math_test.go
+++ math_test.go
@@ -17,3 +17,11 @@ func TestSub(t *testing.T) {
 		t.Error("Resultado é errado, valor esperado era ")
 	}
 }
+
+func TestPrint(t *testing.T) {
+
+	total := main()
+	if total != "" {
+		t.Error("Resultado é errado, valor esperado era ")
+	}
+}
